3b507451abc7c021200fa161cd57e08f92bfb61c
KarMur10 <60687016+KarMur10@users.noreply.github.com>
2020-02-05 09:49:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,19 +4,29 @@ using namespace std;
 
 int main()
 {
+    string eilute1, eilute2, eilute3, eilute4, eilute5;
     string vardas;
     cout << "iveskite varda\n";
     cin >> vardas;
     for(int i = 0; i < 13+vardas.length(); i++)
     {
         cout << "*";
+        eilute1 += '*';
     }
     cout << endl << "*";
+    eilute2 += '*';
     for(int a = 0; a < 11+vardas.length(); a++)
     {
         cout << " ";
+        eilute2 += ' ';
     }
+    eilute2 += '*';
     cout << "*" << endl << "* Sveikas, " << vardas << " *" << endl << "*";
+    eilute3 += "* Sveikas, ";
+    eilute3 += vardas;
+    eilute3 += " *";
+    eilute4 = eilute2;
+    eilute5 = eilute1;
     for(int c = 0; c < 11+vardas.length(); c++)
     {
         cout << " ";
@@ -26,6 +36,11 @@ int main()
     {
         cout << "*";
     }
-    cout << endl;
+    cout << endl << endl;
+    cout << eilute1 << endl;
+    cout << eilute2 << endl;
+    cout << eilute3 << endl;
+    cout << eilute4 << endl;
+    cout << eilute5 << endl;
     return 0;
 }
