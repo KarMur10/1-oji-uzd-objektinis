4518ce21e5d9b004324b6a3ca61a360fcb55630b
KarMur10 <60687016+KarMur10@users.noreply.github.com>
2020-02-13 21:56:44 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    bool lytis;
+    int t;
     string eilute1, eilute2, eilute3, eilute4, eilute5;
     string vardas;
     cout << "iveskite varda\n";
     cin >> vardas;
-    for(int i = 0; i < 13+vardas.length(); i++)
+    if (vardas[vardas.length() - 1] == 's')
+    {
+        lytis = 1;
+        t = 11;
+    }
+    else
+    {
+        lytis = 0;
+        t = 10;
+    }
+    cout << lytis << endl;
+    for(int i = 0; i < t+2+vardas.length(); i++)
     {
         cout << "*";
         eilute1 += '*';
     }
     cout << endl << "*";
     eilute2 += '*';
-    for(int a = 0; a < 11+vardas.length(); a++)
+    for(int a = 0; a < t+vardas.length(); a++)
     {
         cout << " ";
         eilute2 += ' ';
     }
     eilute2 += '*';
-    cout << "*" << endl << "* Sveikas, " << vardas << " *" << endl << "*";
+    cout << "*" << endl;
+    if (lytis == 1) cout << "* Sveikas, ";
+    else cout << "* Sveika, ";
+    cout << vardas << " *" << endl;
+    cout << "*";
     eilute3 += "* Sveikas, ";
     eilute3 += vardas;
     eilute3 += " *";
     eilute4 = eilute2;
     eilute5 = eilute1;
-    for(int c = 0; c < 11+vardas.length(); c++)
+    for(int c = 0; c < t+vardas.length(); c++)
     {
         cout << " ";
     }
     cout << "*" << endl;
-    for(int q = 0; q < 13+vardas.length(); q++)
+    for(int q = 0; q < t+2+vardas.length(); q++)
     {
         cout << "*";
     }
-    cout << endl << endl;
-    cout << eilute1 << endl;
-    cout << eilute2 << endl;
-    cout << eilute3 << endl;
-    cout << eilute4 << endl;
-    cout << eilute5 << endl;
     return 0;
 }
